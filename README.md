56275f7aa8cc7962d24d02f3bcbad3695b5c6b6a
George <gogent01@gmail.com>
2024-01-22 10:25:32 +0100

    Update README and CLI command descriptions

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # cars-db-cli
 
 This project represents a simple CLI for database API with information about various cars. Supports user authentication and CRUD operations upon the car database for authorized users.
+
+## How to install
+
+```bash
+# 1. Install packages
+npm install
+
+# 2. Install ts-node globally and run index.ts
+npm install -g ts-node
+ts-node index.ts <command> [arguments]
+
+# 3. Alternatively, run build command and execute index.js from the build folder
+npm install -g typescript
+tsc
+cd dist
+node index.js <command> [arguments]
+```
+
+## How to use
+
+This CLI provides 4 commands to interact with the car database: `add`, `find`, `update`, and `delete`. Help on arguments for each command can be found by using --help option:
+```bash
+ts-node index.ts <command> --help
+```
+For convenience, repository scripts contain preconfigured commands for each CRUD operation: `add-car`, `find-cars`, `update-car`, `delete-car`. You can use them like this:
+```bash
+npm run add-car
+# ts-node index.ts add -e user@example.com -p P4ssword! --brand Citroen --model C4 --year 2020 --price 2600000
+```
--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-import { App } from './src/App';
 import { Command, Option } from 'commander';
+import { App } from './src/App';
 import { Car } from './src/types';
 
 const p = new Command();
 
 p.command('add')
+  .description('Add a car to the database, providing a car brand, model, year and price in RUB.')
   .requiredOption('-e, --email <email>', 'E-mail for authentication')
   .requiredOption('-p, --password <password>', 'Password for authentication')
-  .requiredOption('-b, --brand <brand>', "A car's brand")
-  .requiredOption('-m, --model <model>', "A car's model")
-  .requiredOption('-y, --year <year>', "A car's manufacturing year")
-  .requiredOption('-c, --price <price>', "A car's price")
+  .requiredOption('-b, --brand <brand>', 'Car brand (required)')
+  .requiredOption('-m, --model <model>', 'Car model (required)')
+  .requiredOption('-y, --year <year>', 'Car manufacturing year (required)')
+  .requiredOption('-c, --price <price>', 'Car price (required)')
   .action(async (options, command) => {
     const app = new App({ email: options.email, password: options.password });
     const car: Car = {
@@ -31,12 +32,15 @@ p.command('add')
   });
 
 p.command('find')
+  .description(
+    'Find cars that match filters for brand, model, year and price. The output can be sorted by brand, model, year or price in ascending or descending order. By default, no filters are applied and the output is sorted by brand name in ascending order.'
+  )
   .requiredOption('-e, --email <email>', 'E-mail for authentication')
   .requiredOption('-p, --password <password>', 'Password for authentication')
-  .option('-b, --brand <brand>', "A car's brand")
-  .option('-m, --model <model>', "A car's model")
-  .option('-y, --year <year>', "A car's manufacturing year")
-  .option('-c, --price <price>', "A car's price")
+  .option('-b, --brand <brand>', 'Car brand')
+  .option('-m, --model <model>', 'Car model')
+  .option('-y, --year <year>', 'Car manufacturing year')
+  .option('-c, --price <price>', 'Car price')
   .addOption(
     new Option('-s, --sortField <sortField>', 'A field used for sorting').choices(['brand', 'model', 'year', 'price'])
   )
@@ -56,13 +60,14 @@ p.command('find')
   });
 
 p.command('update')
+  .description('Update a car in the database by id, providing brand, model, year and price in RUB.')
   .requiredOption('-e, --email <email>', 'E-mail for authentication')
   .requiredOption('-p, --password <password>', 'Password for authentication')
-  .requiredOption('-i, --id <id>', "A car's id")
-  .requiredOption('-b, --brand <brand>', "A car's brand")
-  .requiredOption('-m, --model <model>', "A car's model")
-  .requiredOption('-y, --year <year>', "A car's manufacturing year")
-  .requiredOption('-c, --price <price>', "A car's price")
+  .requiredOption('-i, --id <id>', 'Car id (required)')
+  .requiredOption('-b, --brand <brand>', 'Car brand (required)')
+  .requiredOption('-m, --model <model>', 'Car model (required)')
+  .requiredOption('-y, --year <year>', 'Car manufacturing year (required)')
+  .requiredOption('-c, --price <price>', 'Car price (required)')
   .action(async (options, command) => {
     const car: Required<Car> = {
       _id: options.id,
@@ -84,9 +89,10 @@ p.command('update')
   });
 
 p.command('delete')
+  .description('Delete a car from the database by id.')
   .requiredOption('-e, --email <email>', 'E-mail for authentication')
   .requiredOption('-p, --password <password>', 'Password for authentication')
-  .requiredOption('-i, --id <id>', "A car's id")
+  .requiredOption('-i, --id <id>', 'Car id (required)')
   .action(async (options, command) => {
     const app = new App({ email: options.email, password: options.password });
     const deletedCar = await app.delete(options.id).catch((error) => {
